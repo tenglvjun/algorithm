9e5e3715943ea34b934fe468d41c336b1e07f784
Tony <tenglvjun@gmail.com>
2019-03-06 21:06:32 +0800

    merge sort

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,6 +1,9 @@
 #include "sort.h"
 #include <assert.h>
 #include "tools.h"
+#include <memory.h>
+#include "macro.h"
+#include <iostream>
 
 void InsertSort(int *buf, int len, bool maximum /* = true */)
 {
@@ -50,4 +53,69 @@ void InsertSort(int *buf, int len, bool maximum /* = true */)
 
 void MergeSort(int *buf, int len, bool maximum /* = true */)
 {
+    int middle = len / 2;
+
+    if (middle == 0)
+    {
+        return;
+    }
+
+    int l_len = middle;
+    int r_len = len - middle;
+
+    int *left = new int[l_len];
+    int *right = new int[r_len];
+
+    memcpy(left, buf, sizeof(int) * l_len);
+    memcpy(right, (buf + l_len), sizeof(int) * r_len);
+
+    MergeSort(left, l_len, maximum);
+    MergeSort(right, r_len, maximum);
+
+    if (l_len == 0)
+    {
+        memcpy(buf, right, sizeof(int) * r_len);
+        return;
+    }
+
+    if (r_len == 0)
+    {
+        memcpy(buf, left, sizeof(int) * l_len);
+        return;
+    }
+
+    int i, j, k;
+    i = j = 0;
+    k = i + j;
+
+    while (k < len)
+    {
+        if (i == l_len)
+        {
+            memcpy(buf + k, right + j, sizeof(int) * (r_len - j));
+            break;
+        }
+
+        if (j == r_len)
+        {
+            memcpy(buf + k, left + i, sizeof(int) * (l_len - i));
+            break;
+        }
+
+        if (left[i] < right[j])
+        {
+            buf[k++] = right[j];
+            j++;
+        }
+        else
+        {
+            buf[k++] = left[i];
+            i++;
+        }
+    }
+
+    SAFE_DELETE_ARRAY(left);
+    SAFE_DELETE_ARRAY(right);
+
+    return;
 }
